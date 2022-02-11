7231050feef925526ecddf66b12f35df51ae0ef9
Dariastich <94920518+Dariastich@users.noreply.github.com>
2022-02-11 19:44:42 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import pygame
+import resquests
+
+
+def load_map(object):
+    map_params = {
+        "ll": object.ll(),
+        "l": object.type,
+        "z": object.zoom,
+    }
+
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    response = requests.get(map_api_server, params=map_params)
+
+    if not response:
+        print("Ошибка")
+        sys.exit(1)
+
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+
+    return map_file
+
+
+
+class MapObject():
+    def __init__(self):
+        self.lat = 55.733842
+        self.lon = 37.588144
+        self.zoom = 16
+        self.type = 'map'
+
+    def ll(self):
+        return str(sellf.lon) + ',' + str(self.lat)
+
+pygame.init()
+screen = pygame.dsplay.set_mode((600, 450))
+
+map = MapObject()
+
+
+while True():
+    event = pygame.event.ait()
+    if event.type == pygame.QUIT:
+        break
+
+    map_file = load_map(map)
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    pygame.display.flip()
+
+pygame.quit()
+os.remove(map_file)
